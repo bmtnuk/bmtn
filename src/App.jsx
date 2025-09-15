bb853ed27a029ee3b3e6e729c5b00fffba82de38
Yozora XCII <yozoraxcii@gmail.com>
2025-09-15 09:47:36 +0100

    Deploy BMTN splash PWA

---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+export default function App() {
+  return (
+    <div className="flex flex-col items-center justify-center h-screen text-center text-white">
+      <h1 className="font-audiowide text-6xl mb-4">BMTN</h1>
+      <p className="text-xl">Coming Soon</p>
+    </div>
+  );
+}
