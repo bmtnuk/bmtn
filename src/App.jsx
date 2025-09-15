20aa6decb183a141661e969c3d2f6249b5f14d98
Yozora XCII <yozoraxcii@gmail.com>
2025-09-15 11:32:19 +0100

    Deploy splash screen

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ export default function App() {
   return (
     <div className="flex flex-col items-center justify-center h-screen text-center text-white">
       <h1 className="font-audiowide text-6xl mb-4">BMTN</h1>
-      <p className="text-xl">Coming Soon</p>
+      <p className="text-xl">Badminton Rating System - Coming Soon</p>
     </div>
   );
 }
